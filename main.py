fe769a66cd85434bb8928c1739c689a77a03bd9f
Keion Yan <jay.walk3r911@gmail.com>
2025-11-19 19:34:00 +0000

    ok

--- main.py
+++ main.py
@@ -1,1 +1,22 @@
 
+list_of_students_out = ["Tommy","Andy","Sebastian","Allen", "Ben"]
+
+list_of_time_out = ["7","4","3","6","20"]
+time = dict(zip(list_of_students_out), list_of_time_out)
+
+if time < (5):
+ print("You are safe")
+if time in range(6,10):
+    print("You have been flagged for being out for an unnecessary amount of time.")
+if time in range(11,60):
+   print ("You ahve been flagged for being out for an unnecessary amount of time.")
+
+
+
+if list_of_time_out > five:
+    print(warning)
+print(list)
+
+time = dict(zip(list_of_students_out), list_of_time_out)
+
+moods = dict(zip(list1, list2))
