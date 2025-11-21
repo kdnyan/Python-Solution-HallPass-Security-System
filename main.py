5b36c163da99c6268c0caf52bbc4656d649c8135
Keion Yan <jay.walk3r911@gmail.com>
2025-11-21 19:16:52 +0000

    okay

--- main.py
+++ main.py
@@ -1,22 +1,46 @@
 
+# list_of_students_out = ["Tommy","Andy","Sebastian","Allen", "Ben"]
+
+# list_of_time_out = ["7","4","3","6","20"]
+# time = zip(list_of_students_out, list_of_time_out)
+# print (time)
+# if time < (5):
+#  print("You are safe")
+# if time in range(6,10):
+#     print("You have been flagged for being out for an unnecessary amount of time.")
+# if time in range(11,60):
+#    print ("You ahve been flagged for being out for an unnecessary amount of time.")
+
+
+
+# if list_of_time_out > five:
+#     print(warning)
+# print(list)
+
+# time = dict(zip(list_of_students_out), list_of_time_out)
+
+# moods = dict(zip(list1, list2))
+
 list_of_students_out = ["Tommy","Andy","Sebastian","Allen", "Ben"]
 
-list_of_time_out = ["7","4","3","6","20"]
-time = dict(zip(list_of_students_out), list_of_time_out)
+list_of_time_out = [7,4,3,6,20]
 
-if time < (5):
- print("You are safe")
-if time in range(6,10):
-    print("You have been flagged for being out for an unnecessary amount of time.")
-if time in range(11,60):
-   print ("You ahve been flagged for being out for an unnecessary amount of time.")
+# Create a dictionary: {student: minutes_out}
+time_out_dict = dict(zip(list_of_students_out, list_of_time_out))
+print(time_out_dict)
 
+# Loop through each student and their time
+for student, minutes in time_out_dict.items():
 
+    if minutes < 5:
+        print(f"{student}: You are safe.")
 
-if list_of_time_out > five:
-    print(warning)
-print(list)
+    elif 5 <= minutes < 10:
+        print(f"{student}: You have been flagged for being out unnecessarily.")
 
-time = dict(zip(list_of_students_out), list_of_time_out)
+    elif 10 <= minutes < 60:
+        print(f"{student}: You have been flagged for being out for a long time.")
 
-moods = dict(zip(list1, list2))
+    else:
+        print(f"{student}: EXTREME time out. Further conversation needed.")
+
